bb90605f1258525c2350b20e144daaa3362d8326
Lesmes <20rasputin03@gmail.com>
2025-07-25 14:02:51 -0500

    ADD: progres bar in API

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,5 +1,8 @@
 # lotery to find in API.py
 FIND_LOTERY = 'ASTRO'
+API_URL = "https://api-resultadosloterias.com/api/results/"
+FECHA_DEFECTO = "2023-02-01"
+CLAVES_UNICAS = ["lottery", "slug", "date", "result", "series"]
 
 # Iteration for prediction.py
 ITERATIONS = 3000
